1f0987005d7e70775b4b27b7ac02191c0b34bad4
Ugochukwu Victor <hugotchee1@gmail.com>
2024-05-14 22:18:48 +0100

    added loading page

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoadingPage = () => {
+  return (
+    <div className='loading'>
+        <h3>WRITE</h3>
+        
+    </div>
+  )
+}
+
+export default LoadingPage
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,12 @@
 import React, {Suspense, lazy} from 'react'
 import { Link } from 'react-router-dom'
+import LoadingPage from '../components/LoadingPage'
 
 const Home = () => {
   const Body = lazy(() => import('../components/HomeBody'))
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<LoadingPage />}>
         <Body />
       </Suspense>
     </div>
