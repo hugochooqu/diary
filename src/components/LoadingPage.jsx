d49fe92354951525260653b7ba797572da8f1001
Ugochukwu Victor <hugotchee1@gmail.com>
2024-05-14 22:34:10 +0100

    install react loader spinner

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { ThreeDots } from 'react-loader-spinner'
 
 const LoadingPage = () => {
   return (
     <div className='loading'>
         <h3>WRITE</h3>
-        
+        <ThreeDots 
+            visible={true}
+            secondaryColor='black'
+            color='grey'
+        />
     </div>
   )
 }
