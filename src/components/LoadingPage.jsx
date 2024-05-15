b84b586fe2075fca27bdbb31a889cf696f56e078
Ugochukwu Victor <hugotchee1@gmail.com>
2024-05-15 10:11:07 +0100

    changed loding dot

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { ThreeDots } from 'react-loader-spinner'
+import React from "react";
+import { ThreeDots } from "react-loader-spinner";
 
 const LoadingPage = () => {
   return (
-    <div className='loading'>
+    <div className="loading">
+      <div className="loading-body">
         <h3>WRITE</h3>
-        <ThreeDots 
-            visible={true}
-            secondaryColor='black'
-            color='grey'
-        />
+        <ThreeDots height="80"
+  width="80"
+  color="grey"
+  radius="9"
+  ariaLabel="three-dots-loading"
+  wrapperStyle={{}}
+  wrapperClass=""/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoadingPage
+export default LoadingPage;
